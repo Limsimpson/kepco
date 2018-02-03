a36cb73a075cd024a10ff412d2e4947c220e9971
임정임 <hisimpson90@gmail.com>
2018-02-04 00:53:07 +0900

    containerInfo의 board ul 높이값 수정, rollingBan library를 이용해 css, jquery 수정, 추가

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
-// TAB메뉴
-
 $(function () {
+
+    // TAB메뉴
+
     $(".tab_content").hide();
     $(".tab_content:first").show();
 
     $("ul.tabs li").click(function () {
-        $(this).siblings("li").removeClass("selected");
-        //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
-        $(this).addClass("selected");
-        $(".tab_content").hide();
-        var activeTab = $(this).attr("rel");
-        $("#" + activeTab).fadeIn();
+        if ($(this).hasClass('more')) {
+            // window.open('http://asddasd.com', '_self')
+            return;
+        } else {
+            $(this).siblings("li").removeClass("selected");
+            //$(this).addClass("active").css({"color": "darkred","font-weight": "bolder"});
+            $(this).addClass("selected");
+            $(".tab_content").hide();
+            var activeTab = $(this).attr("rel");
+            $("#" + activeTab).fadeIn();
+        }
+    });
+
+    // ROLLING BANNER
+
+    $(".rollingBan").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
 });
-
-// ROLLING BANNER
